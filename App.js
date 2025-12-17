e129b01c1b482249e1348f6733ae90e9266afc05
Diego Araújo Teixeira <diegoaraujoki@gmail.com>
2025-12-17 20:25:25 -0300

    subindo os arquivos para unificar

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+import PaginaPrincipal from './src/Paginas/PaginaPrincipal';
+import PaginaConversa from './src/Paginas/PaginaConversa';
+import PaginaPerfil from './src/Paginas/PaginaPefil'; 
+import TelaMensagens from './src/Paginas/TelaMensagens';
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function ChatStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="ListaConversas" 
+        component={PaginaConversa} 
+        options={{ headerShown: false }} 
+      />
+      <Stack.Screen 
+        name="Mensagens" 
+        component={TelaMensagens} 
+        options={({ route }) => ({ title: route.params.nome })} 
+      />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Início') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Chat') {
+              iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+            } else if (route.name === 'Perfil') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#555151',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false, 
+        })}
+      >
+        <Tab.Screen name="Chat" component={ChatStack} />
+        <Tab.Screen name="Início" component={PaginaPrincipal} />
+        <Tab.Screen name="Perfil" component={PaginaPerfil} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/Paginas/PaginaPefil.js
@@ -0,0 +1,31 @@
+import { Text, View, StyleSheet,FlatList } from 'react-native'
+
+export default function PaginaConversa(){
+    return<>
+    {/*Header*/}
+    <View style={style.header}>
+        <Text style={style.headerText}>Perfil</Text>
+    </View>
+
+    {/*Body*/}
+    <View>
+        <Text style={style.text}>Perfil</Text>
+    </View>
+    {/*Footer*/}
+    </>
+}
+const style = StyleSheet.create({
+    header:{
+        width : '100%',
+        backgroundColor: "#555151ff",
+
+    },
+    headerText:{
+        color: "#fff    "
+    },
+    text:{
+        fontSize: 50,
+        alignItems:'center',
+        textAlign: 'center'
+    }
+});
